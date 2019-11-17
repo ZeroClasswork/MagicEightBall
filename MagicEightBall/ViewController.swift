3926363423b8e1e3635798954eeff920da5a8904
atleastzero <megan.obryan52@gmail.com>
2019-11-17 14:30:45 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Megan OBryan on 11/17/19.
+//  Copyright © 2019 Makeschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
