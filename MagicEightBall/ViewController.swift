8b59e5c2d05647ac1dab04bb97721150cba10e5a
atleastzero <megan.obryan52@gmail.com>
2019-11-20 09:26:41 -0800

    Got it working, finally!

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let answers = ["Fosho", "Never", "That's sus", "Hmm... I dunno about that one...", "There's nothing I'm more certain about"]
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateAnswer()
+    }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
 
+        generateAnswer()
+    }
+    
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+
+        answerLabel.text = answers[randomIndex]
+    }
 }
 
